ce23202ff29844b8287fc2f5e5be160f89b71369
an.mai <an.mai@sutrixsolutions.com>
2018-06-29 19:32:30 +0700

    [Add Firebase Config]

--- foodorder/src/app/app.module.ts
+++ foodorder/src/app/app.module.ts
@@ -6,6 +6,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAP276Ycpd_K-WnndBo2a8e0KgRix9iWVo",
+  authDomain: "foodorder-b2fea.firebaseapp.com",
+  databaseURL: "https://foodorder-b2fea.firebaseio.com",
+  projectId: "foodorder-b2fea",
+  storageBucket: "foodorder-b2fea.appspot.com",
+  messagingSenderId: "965861509741"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +26,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
