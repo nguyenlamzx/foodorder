a195186c95b5468147a911b05786de0a87cf3f8f
Lam Nguyen <lam.nv@sutrixsolutions.com>
2018-07-18 13:44:26 +0700

    update for pages & refactor code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,46 +4,20 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { TranslateService } from '@ngx-translate/core';
 import { Config, Nav, Platform } from 'ionic-angular';
 
-import { FirstRunPage } from '../pages';
+import { FirstRunPage, pages } from '../pages';
 import { Settings } from '../providers';
 
+import toArray from "lodash/toArray";
+
 @Component({
-  template: `<ion-menu [content]="content">
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Pages</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
-    <ion-content>
-      <ion-list>
-        <button menuClose ion-item *ngFor="let p of pages" (click)="openPage(p)">
-          {{p.title}}
-        </button>
-      </ion-list>
-    </ion-content>
-
-  </ion-menu>
-  <ion-nav #content [root]="rootPage"></ion-nav>`
+  templateUrl: './app.component.html'
 })
 export class MyApp {
   rootPage = FirstRunPage;
 
   @ViewChild(Nav) nav: Nav;
 
-  pages: any[] = [
-    { title: 'Tutorial', component: 'TutorialPage' },
-    { title: 'Welcome', component: 'WelcomePage' },
-    { title: 'Tabs', component: 'TabsPage' },
-    { title: 'Cards', component: 'CardsPage' },
-    { title: 'Content', component: 'ContentPage' },
-    { title: 'Login', component: 'LoginPage' },
-    { title: 'Signup', component: 'SignupPage' },
-    { title: 'Master Detail', component: 'ListMasterPage' },
-    { title: 'Menu', component: 'MenuPage' },
-    { title: 'Settings', component: 'SettingsPage' },
-    { title: 'Search', component: 'SearchPage' }
-  ]
+  pages: any = toArray(pages);
 
   constructor(private translate: TranslateService, platform: Platform, settings: Settings, private config: Config, private statusBar: StatusBar, private splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,11 +1,25 @@
+export const pages = {
+  tutorial: { title: 'Tutorial', component: 'TutorialPage' },
+  welcome: { title: 'Welcome', component: 'WelcomePage' },
+  tabs: { title: 'Tabs', component: 'TabsPage' },
+  cards: { title: 'Cards', component: 'CardsPage' },
+  content: { title: 'Content', component: 'ContentPage' },
+  login: { title: 'Login', component: 'LoginPage' },
+  signup: { title: 'Signup', component: 'SignupPage' },
+  master: { title: 'Master Detail', component: 'ListMasterPage' },
+  menu: { title: 'Menu', component: 'MenuPage' },
+  settings: { title: 'Settings', component: 'SettingsPage' },
+  search: { title: 'Search', component: 'SearchPage' }
+};
+
 // The page the user lands on after opening the app and without a session
-export const FirstRunPage = 'TutorialPage';
+export const FirstRunPage = pages.tutorial;
 
 // The main page the user will see as they use the app over a long period of time.
 // Change this if not using tabs
-export const MainPage = 'TabsPage';
+export const MainPage = pages.tabs;
 
 // The initial root pages for our tabs (remove if not using tabs)
-export const Tab1Root = 'ListMasterPage';
-export const Tab2Root = 'SearchPage';
-export const Tab3Root = 'SettingsPage';
+export const Tab1Root = pages.master;
+export const Tab2Root = pages.search;
+export const Tab3Root = pages.settings;
